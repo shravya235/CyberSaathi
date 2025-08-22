7ea2673c92b3060fb9c0fcb7455c61d77f0a8a63
sandeep pai <sandeeppaikulyadi05@gmail.com>
2025-08-22 22:22:47 +0530

    updated navbar

---
+++ cybersaathi/src/components/Navbar/Navlinks.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function NavLinks({ closeMenu }) {
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "Quiz", href: "/quiz" },
+    { name: "About", href: "/about" },
+    { name: "Community", href: "/community" },
+  ];
+
+  return (
+    <>
+      {links.map(({ name, href }) => (
+        <Link
+          key={name}
+          href={href}
+          className="hover:text-blue-700 font-medium"
+          onClick={closeMenu}
+        >
+          {name}
+        </Link>
+      ))}
+    </>
+  );
+}
