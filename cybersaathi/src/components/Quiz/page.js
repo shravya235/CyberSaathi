fe906fc003ae9fa8cb24ddd1f20dbf288833b59d
shreeRCode <mshreeraksha3@gmail.com>
2025-08-23 10:03:27 +0530

    Routing for demographicform

---
+++ cybersaathi/src/components/Quiz/page.js
@@ -0,0 +1,10 @@
+import DemographicForm from "../components/Quiz/DemographicForm";
+
+export default function QuizStartPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Quiz: Select Your Demographic</h2>
+      <DemographicForm />
+    </div>
+  );
+}
